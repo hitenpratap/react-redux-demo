7a5bc2443d8a2be79c231540dfa42a1c921dc048
Hiten Pratap Singh <hitenpratap99@gmail.com>
2018-04-25 13:10:56 +0530

    Removed commented code

--- src/index.js
+++ src/index.js
@@ -1,20 +1,14 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import './index.css';
-// import App from './App';
-// import registerServiceWorker from './registerServiceWorker';
-
-// ReactDOM.render(<App />, document.getElementById('root'));
-// registerServiceWorker();
-
 import React from "react";
 import { render } from "react-dom";
 import { Provider } from "react-redux";
 import store from "./js/store/index";
 import App from "./components/App";
+import registerServiceWorker from './registerServiceWorker';
+
 render(
   <Provider store={store}>
     <App />
   </Provider>,
   document.getElementById("root")
 );
+registerServiceWorker();
